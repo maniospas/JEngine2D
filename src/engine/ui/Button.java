b511731f0d89f9c4b7fce3356d5bbae03ce223f9
manios krasanakis <maniospas@hotmail.com>
2023-06-24 02:09:04 +0300

    more organized tooltips

--- src/engine/ui/Button.java
+++ src/engine/ui/Button.java
@@ -25,9 +25,6 @@ public class Button extends UIEntity {
 	private boolean enabled = true;
 	private boolean visible = true;
 	private String tooltip = null;
-	private static Font titleFont = StyleManager.deriveFont(Font.BOLD, 27);
-	private static Font tooltipFont = StyleManager.deriveFont(Font.PLAIN, 20);
-	private static Font smallFont = StyleManager.deriveFont(Font.PLAIN, 16);
 	
 	public Button setText(String text) {
 		this.text = text;
@@ -123,103 +120,10 @@ public class Button extends UIEntity {
 			int x = (int)this.x-(isEntered?enteredSizeIncrease:0);
 			int y = (int)this.y-(isEntered?enteredSizeIncrease:0);
 			int dx = 88;//this.dx+2*(isEntered?enteredSizeIncrease:0);
-			int dy = 80;//this.dy+2*(isEntered?enteredSizeIncrease:0);
-			String[] lines = tooltip.split("\\n");
-			dy = 20*lines.length+dx/20;
+			//int dy = 80;//this.dy+2*(isEntered?enteredSizeIncrease:0);
 			int px = x+(int)this.dx/2-dx*3/2+20;
-			int py = y-3*dy/2-10;
-			g.setColor(new Color(0,0,0,200));
-			g.fillRoundRect(px, py, 3*dx-40, 3*dy/2, dx/5, dx/5);
-			g.setColor(Color.black);
-			g.drawRoundRect(px, py, 3*dx-40, 3*dy/2, dx/5, dx/5);
-			py += dx/20;
-			py += 22;
-			for(String line : lines) {
-				g.setColor(Color.white); 
-				int lineX = px+5;
-				if(line.startsWith("# ")) {
-					line = line.substring(2);
-					g.setFont(titleFont);
-					lineX = px+(3*dx-40)/2-g.getFontMetrics().stringWidth(
-							line.split("\\[")[0])/2;
-				}
-				else 
-					g.setFont(tooltipFont);
-				Font normalFont = g.getFont();
-				
-				//g.drawString(line, px+5, py);
-				String word = "";
-				for(int i=0;i<line.length();i++) {
-					char c = line.charAt(i);
-					if(c>=128) {//non-ascii
-						g.drawString(word, lineX, py);
-						lineX += g.getFontMetrics().stringWidth(word+" ");
-						word = "";
-						Font prevFont = g.getFont();
-						g.setFont(StyleManager.deriveFallbackFont(prevFont));
-						g.drawString(""+c, lineX, py);
-						lineX += g.getFontMetrics().stringWidth(""+c);
-						g.setFont(prevFont);
-					}
-					else if(c=='[') {
-						g.drawString(word, lineX, py);
-						lineX += g.getFontMetrics().stringWidth(word);
-						word = ""+c;
-					}
-					else if(c==']') {
-						word += c;
-						if(word.equals("[white]"))
-							g.setColor(Color.white);
-						if(word.equals("[black]"))
-							g.setColor(Color.black);
-						if(word.equals("[red]"))
-							g.setColor(Color.red);
-						if(word.equals("[green]"))
-							g.setColor(Color.green);
-						if(word.equals("[yellow]"))
-							g.setColor(Color.yellow);
-						if(word.equals("[gray]"))
-							g.setColor(Color.lightGray);
-						if(word.equals("[blue]"))
-							g.setColor(Color.cyan);
-						if(word.equals("[small]")) 
-							g.setFont(normalFont==tooltipFont?smallFont:tooltipFont);
-						if(word.equals("[normal]")) 
-							g.setFont(normalFont);
-						
-						word = "";
-					}
-					else if(c==' ') {
-						if(word.startsWith("{") && word.endsWith("}")) {
-							Texture texture = TextureLoader.get(word.substring(1, word.length()-1));
-							int lineHeight = g.getFontMetrics().getHeight()*4/5;
-							texture.draw(g, lineX, py-lineHeight, lineHeight, lineHeight);
-							lineX += lineHeight;
-						}
-						else {
-							g.drawString(word, lineX, py);
-							lineX += g.getFontMetrics().stringWidth(word+" ");
-						}
-						word = "";
-					}
-					else
-						word += c;
-				}
-				if(!word.isEmpty()) {
-					if(word.startsWith("{") && word.endsWith("}")) {
-						Texture texture = TextureLoader.get(word.substring(1, word.length()-1));
-						int lineHeight = g.getFontMetrics().getHeight()*4/5;
-						texture.draw(g, lineX, py-lineHeight, lineHeight, lineHeight);
-					}
-					else
-						g.drawString(word, lineX, py);
-				}
-				if(word.endsWith(".") || normalFont==titleFont)
-					py += 10;
-				if(line.isEmpty())
-					py -= 10;
-				py += 23;
-			}
+			int py = y-10;
+			FancyDraw.fancyDraw(g, tooltip, px, py, dx, false);
 		}
 	}
 	
---
+++ src/engine/ui/FancyDraw.java
@@ -0,0 +1,129 @@
+package engine.ui;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+
+import engine.textures.Texture;
+import engine.textures.TextureLoader;
+
+public class FancyDraw {
+	private static Font titleFont = StyleManager.deriveFont(Font.BOLD, 27);
+	private static Font tooltipFont = StyleManager.deriveFont(Font.PLAIN, 20);
+	private static Font smallFont = StyleManager.deriveFont(Font.PLAIN, 16);
+	
+	protected static void fancyDraw(Graphics g, String tooltip, 
+			int px, int py, int dx, boolean fromTop) {
+		int height = fancyDraw(g, tooltip, px, py, dx, 0);
+		if(!fromTop)
+			py -= height;
+		fancyDraw(g, tooltip, px, py, dx, height);
+	}
+	
+	protected static int fancyDraw(Graphics g, String tooltip, 
+			int px, int py, int dx, int dy) {
+		int startpy = py;
+		String[] lines = tooltip.split("\\n");
+		g.setColor(new Color(0,0,0,200));
+		if(dy!=0) {
+			g.fillRoundRect(px, py, 3*dx-40, dy, dx/5, dx/5);
+			g.setColor(Color.black);
+			g.drawRoundRect(px, py, 3*dx-40, dy, dx/5, dx/5);
+		}
+		py += dx/20;
+		py += 22;
+		for(String line : lines) {
+			g.setColor(Color.white); 
+			int lineX = px+5;
+			if(line.startsWith("# ")) {
+				line = line.substring(2);
+				g.setFont(titleFont);
+				lineX = px+(3*dx-40)/2-g.getFontMetrics().stringWidth(
+						line.split("\\[")[0])/2;
+			}
+			else 
+				g.setFont(tooltipFont);
+			Font normalFont = g.getFont();
+			
+			//g.drawString(line, px+5, py);
+			String word = "";
+			for(int i=0;i<line.length();i++) {
+				char c = line.charAt(i);
+				if(c>=128) {//non-ascii
+					if(dy!=0)
+						g.drawString(word, lineX, py);
+					lineX += g.getFontMetrics().stringWidth(word+" ");
+					word = "";
+					Font prevFont = g.getFont();
+					g.setFont(StyleManager.deriveFallbackFont(prevFont));
+					if(dy!=0)
+						g.drawString(""+c, lineX, py);
+					lineX += g.getFontMetrics().stringWidth(""+c);
+					g.setFont(prevFont);
+				}
+				else if(c=='[') {
+					if(dy!=0)
+						g.drawString(word, lineX, py);
+					lineX += g.getFontMetrics().stringWidth(word);
+					word = ""+c;
+				}
+				else if(c==']') {
+					word += c;
+					if(word.equals("[white]"))
+						g.setColor(Color.white);
+					if(word.equals("[black]"))
+						g.setColor(Color.black);
+					if(word.equals("[red]"))
+						g.setColor(Color.red);
+					if(word.equals("[green]"))
+						g.setColor(Color.green);
+					if(word.equals("[yellow]"))
+						g.setColor(Color.yellow);
+					if(word.equals("[gray]"))
+						g.setColor(Color.lightGray);
+					if(word.equals("[blue]"))
+						g.setColor(Color.cyan);
+					if(word.equals("[small]")) 
+						g.setFont(normalFont==tooltipFont?smallFont:tooltipFont);
+					if(word.equals("[normal]")) 
+						g.setFont(normalFont);
+					
+					word = "";
+				}
+				else if(c==' ') {
+					if(word.startsWith("{") && word.endsWith("}")) {
+						Texture texture = TextureLoader.get(word.substring(1, word.length()-1));
+						int lineHeight = g.getFontMetrics().getHeight()*4/5;
+						if(dy!=0)
+							texture.draw(g, lineX, py-lineHeight, lineHeight, lineHeight);
+						lineX += lineHeight;
+					}
+					else {
+						if(dy!=0)
+							g.drawString(word, lineX, py);
+						lineX += g.getFontMetrics().stringWidth(word+" ");
+					}
+					word = "";
+				}
+				else
+					word += c;
+			}
+			if(!word.isEmpty()) {
+				if(word.startsWith("{") && word.endsWith("}")) {
+					Texture texture = TextureLoader.get(word.substring(1, word.length()-1));
+					int lineHeight = g.getFontMetrics().getHeight()*4/5;
+					if(dy!=0)
+						texture.draw(g, lineX, py-lineHeight, lineHeight, lineHeight);
+				}
+				else if(dy!=0)
+					g.drawString(word, lineX, py);
+			}
+			if(word.endsWith(".") || normalFont==titleFont)
+				py += 10;
+			if(line.isEmpty())
+				py -= 10;
+			py += 23;
+		}
+		return py - startpy;
+	}
+}
